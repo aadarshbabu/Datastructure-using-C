91e5884a63ce8a8c2d8e4c193ca3127894366ab7
Aadarsh <aadarshsingh121@gmail.com>
2020-11-28 22:23:13 +0530

    Top Function add

--- boblyLinkList.c
+++ boblyLinkList.c
@@ -1,6 +1,5 @@
-#include<stdio.h>
-#include<malloc.h>
-
+#include <stdio.h>
+#include <malloc.h>
 
 struct node
 {
@@ -8,81 +7,172 @@ struct node
     struct node *next, *prv;
 };
 
- struct node* createNode()
- {
-    struct node * temp;
-    temp=malloc(sizeof(struct node));
-    return(temp);
-
- }
-
- struct node *start=NULL;
-
-void insertNode(){
-    struct node * temp;
-    temp=createNode();
-    printf("Enter a value");
-    scanf("%d",&temp->data);
-    temp->next=NULL;
-    temp->prv=NULL;
-
-    if(start==NULL)
-        start=temp;
-
-    else
-    {
-        start->prv=temp;
-        temp->next=start;
-        start=temp;
-        
-    }
-        
-}
-
-deleteFirstNode(){
- struct node * temp;
- if(start==NULL)
- {
-     printf("node is empty");
- }
- else
- { 
-    temp=start;
-    start=start->next;
-    start->prv=NULL;
-    free(temp);   
- }
-
-
-}
-
-void view(){
+struct node *createNode()
+{
     struct node *temp;
-    if(start==NULL)
-    printf("LIst is empty");
+    temp = malloc(sizeof(struct node));
+    printf("Enter a value");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    temp->prv = NULL;
+
+    return (temp);
+}
+
+struct node *start = NULL;
+//Insert Data in at the end..
+
+void insertAtEnd()
+{
+    struct node *NN;
+    struct node *LN;
+    NN = createNode();
+
+    if (start == NULL)
+        start = NN;
+
     else
     {
-        temp=start;
-        while (temp->next!=NULL)
+        LN = start;
+        while (LN->next != NULL)
         {
-            printf("%d \n",temp->data);
-            temp=temp->next;
+            LN = LN->next;
         }
-         printf("%d \n",temp->data);
-        
+        LN->next = NN;
+        NN->prv = LN;
     }
-    
-    
 }
 
-int main(){
-     insertNode();
-     insertNode();
-     insertNode();
-     insertNode();
-     view();
-     deleteFirstNode();
-     view();
-    
+//insert data at the beg.
+struct node *NN;
+void insertAtBeg()
+
+{
+    NN = createNode();
+
+    if (start == NULL)
+    {
+        start = NN;
+    }
+    else
+    {
+        NN->next = start;
+        start->prv = NN;
+        start = NN;
+    }
 }
 
+void deleteLastNode()
+{
+    struct node *LN, *PN;
+    if (start == NULL)
+    {
+        printf("oohoo Link list is empty");
+    }
+    else
+    {
+        LN = start;
+        while (LN->next != NULL)
+        {
+            PN = LN;
+            LN = LN->next;
+        }
+        PN->next = NULL;
+        LN->prv = NULL;
+        free(LN);
+    }
+}
+
+// insert data in Mid
+void insertAtMid()
+{
+    int NodeValue;
+    struct node *CN, *NN;
+    printf("Enter a Node Value to insert new node ");
+    scanf("%d", &NodeValue);
+
+    if (start == NULL)
+    {
+        printf("oohooo Link list is empty First insert some value");
+    }
+    else
+    {
+        CN = start;
+        while (NodeValue != CN->data && CN->next != NULL)
+        {
+            CN = CN->next;
+        }
+        if (CN->next == NULL)
+        {
+            if (CN->data == NodeValue)
+            {
+                printf("Oobhai Last Node value match hear\n");
+                NN=createNode();
+                CN->next=NN;
+                NN->prv=CN;
+            }
+            else
+            {
+                printf("You Node value are not match in Link List");
+                exit(0);
+            }
+        }
+        else
+        {
+            NN = createNode();
+            NN->next = CN->next;
+            NN->next->prv = NN;
+            NN->prv = CN;
+            CN->next = NN;
+        }
+    }
+}
+
+//Deletion at the begnning
+void deleteFirstNode()
+{
+    struct node *temp;
+    if (start == NULL)
+    {
+        printf("node is empty");
+    }
+    else
+    {
+        temp = start;
+        start = start->next;
+        start->prv = NULL;
+        free(temp);
+    }
+}
+//view node data.......
+void view()
+{
+    struct node *temp;
+    if (start == NULL)
+        printf("\nLIst is empty YOU Cant view");
+    else
+    {
+        temp = start;
+        while (temp->next != NULL)
+        {
+            printf("link list data\n");
+            printf("%d \n", temp->data);
+            temp = temp->next;
+        }
+        printf("%d \n", temp->data);
+    }
+}
+
+int main()
+{
+
+    insertAtEnd();
+    insertAtEnd();
+    insertAtEnd();
+    // insertAtEnd();
+    // insertAtEnd();
+    // insertAtEnd();
+    // insertAtMid();
+    insertAtMid();
+    view();
+}
